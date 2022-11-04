9f696a1790e2fdb7822cdfba0bae76650daa9309
LanNetStat <117099688+LanNetStat@users.noreply.github.com>
2022-11-04 23:31:15 +0700

    Size In Byte
    Probably this is a useless script

---
+++ SIB.py
@@ -0,0 +1,7 @@
+import os
+print("Size In Byte. By LanNetStat")
+print("Probably this is a useless script")
+print("Write the path to the file or directory. (For example : D:\\test.txt )")
+sib = input("Path : ")
+print(os.path.getsize(sib))
+print Byte(s)
