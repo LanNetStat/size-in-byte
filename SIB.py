ae8bfc1a7f23951601afb831770e55ca5563c52d
LanNetStat <117099688+LanNetStat@users.noreply.github.com>
2022-11-05 00:23:03 +0700

    Size In Byte
    Probably this is a useless script

--- SIB.py
+++ SIB.py
@@ -1,8 +1,12 @@
 #Probably this is a useless script
 import os
+import time
 print("Size In Byte. By LanNetStat")
 print("Probably this is a useless script")
 print("Write the path to the file or directory. (For example : D:\\test.txt )")
 sib = input("Path : ")
 print(os.path.getsize(sib))
-print ("Byte(s)")
+print("Byte(s)")
+print("Restart after 7 seconds")
+time.sleep(7)
+os.startfile("SIB.py")
